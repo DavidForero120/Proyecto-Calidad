e814b8ca1dccc066ef55f630dab52b5c99d023c3
DavidForero120 <DavidForero120>
2022-10-28 22:24:10 -0500

    validar dinero nulo

--- accion.js
+++ accion.js
@@ -1,27 +1,26 @@
 const boton = document.getElementById('button-addon2');
-
-const confirm = true;
 var opcion = 0; 
 var i = 1;
-
-
 boton.addEventListener('click', ()=>{
       var valor = document.getElementById('inp__').value;
-     
-      while(confirm == true){
-            var rando =  Math.random()*2 + 1;
-            var moneda = rando.toPrecision(1);
-            parseInt(opcion);  
-            opcion = prompt("Eliga: \n 1.cara \n 2.sello");  
-            if(moneda == opcion){         
-                  console.log("usted gano");
-            }else{
-                  console.log("usted perdio");
-            }
-            console.log(`${moneda}  ${i}`);
-            i++
-            confirm == true; 
-
+      if(valor > 1){
+            var confir = true;
+                  while(confir == true){
+                        var rando =  Math.random()*2 + 1;
+                        var moneda = rando.toPrecision(1);
+                        parseInt(opcion);  
+                         opcion = prompt("Eliga: \n 1.cara \n 2.sello");  
+                        if(moneda == opcion){         
+                              window.alert("usted ha ganado");
+                        }else{
+                              window.alert("usted perdio");
+                        }
+                        i++
+                        confir = window.confirm("desea Continuar");     
+                  }
+            window.alert(`Usted jugo ${i} veces y le quedo ${valor}`)
+      }else{
+            window.alert("por favor ingrese un cantidad de dinero a apostar!!")
       }
 
 })
