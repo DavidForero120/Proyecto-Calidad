88743d793b9be9b68a8d20cbd397c1c7cf82a0cb
DavidForero120 <DavidForero120>
2022-10-30 16:10:27 -0500

    funcionalidad terminada

--- accion.js
+++ accion.js
@@ -1,36 +1,40 @@
 const boton = document.getElementById('button-addon2');
 var opcion = 0; 
 var i = 0;
-var total=0;
+
 boton.addEventListener('click', ()=>{
       var valor = document.getElementById('inp__').value;
+      var total= valor;
       if(valor > 1999){
             var confir = true;
+            
                   while(confir == true){
                         i++
                         var rando =  Math.random()*2 + 1;
                         var moneda = rando.toPrecision(1);
-                        parseInt(opcion);  
-                         opcion = prompt("Eliga: \n 1.Cara \n 2.sello");  
-                        if(moneda == opcion){      
-                              total = valor * 2   
+                        parseInt(opcion); 
+                        total = parseInt(total);
+                        valor = parseInt(valor);
+                        opcion = prompt(`Eliga: \n 1. Cara \n 2. Sello`);  
+                        if(moneda == opcion){       
+                              total = total + valor;  
                               window.alert(`¡¡Usted ha ganado!!`);
                         }else{
-                              total = valor - valor 
+                              total = total - valor 
                               window.alert(`¡¡Usted ha perdido!!`);
                         }
-                        console.log(`${total}`)
+                        console.log(`${total}`);
                         
-                        confir = window.confirm("¿desea Continuar?");     
+                        confir = window.confirm("¿Desea Continuar?");     
                   }          
             if(total >0){
-                  window.alert(`Usted jugo ${i} vece(s) y le quedo ${total}`);
-                  window.alert(`Gracias por jugar`);
-            }else{
-                  window.alert(`haz tenido mala suerte perdiste ${valor} pesos`);
-                  window.alert(`Gracias por jugar`);
+                  window.alert(`Usted jugo ${i} vece(s) y le quedo ${total}$ pesos`);
+                  window.alert(`GRACIAS POR JUGAR!!`);
+            }else if(total < 0){
+                  window.alert(`Haz tenido mala suerte perdiste ${total}$ pesos y haz jugado ${i}`);
+                  window.alert(`GRACIAS POR JUGAR!!`);
             }
       }else{
-            window.alert("Por favor ingrese un cantidad de dinero mayor o igual a 2000!!");
+            Swal.fire({title:"Por favor ingrese un cantidad de dinero mayor o igual a 2000!!"});
       }
 })
